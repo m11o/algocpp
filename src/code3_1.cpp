17ec375e1e64f2c87270baaec1cc0ddce6c3a4bd
masahito1997 <velonica.gale2227@gmail.com>
2021-02-01 16:06:04 +0900

    add src

---
+++ src/code3_1.cpp
@@ -0,0 +1,24 @@
+//
+// Created by velonica2227 on 2021/02/01.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N, v;
+    cin >> N >> v;
+    vector<int> a(N);
+
+    for (int i = 0; i < N; ++i) cin >> a[i];
+
+    bool exist = false;
+    for (int i = 0; i < N; ++i) {
+        if (a[i] == v) exist = true;
+    }
+
+    if (exist) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
+
